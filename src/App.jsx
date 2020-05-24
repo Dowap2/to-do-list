18a01e9edf998f26eeae012d082c6e752af12f92
dowap2 <dowapdowari@gmail.com>
2020-05-24 23:31:40 +0900

    Initialization and new creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { DateSelector } from './DateSelector';
-import { ToDoList } from './ToDoList'
-import { AddToDo } from './AddToDo'
-import { Modal } from './Modal'
-import { Close } from './Close'
+import React from 'react';
 
 function App() {
-  let today = new Date();   
+  let arr = [99999999]
+  
+  let today = new Date();  
   let year = today.getFullYear();
   let month = today.getMonth() + 1;
   let date = today.getDate();
-  let fulldate = year + '-' + month + '-' + date;
-  const [todate , setDate] = useState(fulldate);
-  let [todo , setTodo] = useState(0);
-  let [list , setList] = useState([]);
-  let addList = <div>
-    <textarea name="" id="" cols="30" rows="1"></textarea>
-    <button></button>
-  </div>
-  useEffect(() => {
-    if(todo != 0){
-      setList(list = list.concat(addList))
-    }
-  }, [todo])
-  return(
-    <html className="App">
-      <header className="header">
-        <div className="logo_box"></div>
-      </header>
-      <DateSelector value={todate} onChange={setDate}/>
-      <div className="todo_title_box">
-        <p className="todo_title">{todate}의 할일</p>
-      </div>
-      <div className="todo_box">
-        <div>
-          {list}
-        </div>
-      </div>
-      <AddToDo value={todo} onClick={setTodo}/>
-      <button onClick = {e => console.log(todo)}></button>
-    </html>
-  );
+
+  return(<div>
+    <div>투두 리스트</div>
+    <button onClick={e=> console.log()}></button>
+    <div>
+      <div>{year}-{month}-{date}</div>
+      <div>투두리스트 보이는부분</div>
+    </div>
+
+    <div>
+      <div>투두리스트 추가하는부분</div>
+    </div>
+
+  </div>);
 }
 
 export default App;
--- src/ToDoList.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function ToDoList(props){
-    return <div className="date_box">
-        <div className="todo"></div>
-    </div>
-}
